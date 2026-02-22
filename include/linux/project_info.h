b9f944d418bf143906e8d5dfb1b3506eb79c36ed
plushie-neko <sunflowers.warp@gmail.com>
2026-02-22 21:15:01 +0300

    fr1

--- include/linux/project_info.h
+++ include/linux/project_info.h
@@ -10,8 +10,7 @@ typedef     __u8        uint8;
 
 #include <linux/string.h>
 #include <linux/kallsyms.h>
-//extern uint32_t chip_serial_num;
-extern unsigned long totalram_pages; 
+//extern uint32_t chip_serial_num; 
 
 struct project_info {
 	char project_name[8];  //eg, 15801
